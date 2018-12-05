96106172a9da5207ad59444b69149f8ab35badd0
tehlordvortex <tehlordvortex@outlook.com>
2018-12-05 12:03:07 +0100

    add basic app layout
    - adds an header at the top
    - adds a feed list to the bottom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Feed from './Feed';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h4>Feed</h4>
         </header>
+        <main className="App-contents">
+          <Feed />
+        </main>
       </div>
     );
   }
---
+++ src/Feed.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './Feed.css'
+
+class Feed extends Component {
+  render() {
+    return (
+      <div className="Feed">
+      </div>
+    )
+  }
+}
+
+export default Feed;
